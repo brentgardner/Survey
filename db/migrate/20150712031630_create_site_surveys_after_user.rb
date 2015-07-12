57a383061da0218e639c4d858f51f3e2c52b8d05
Brent Gardner <btgardner@gmail.com>
2015-07-12 00:59:28 -0500

    reorg migrations again

---
+++ db/migrate/20150712031630_create_site_surveys_after_user.rb
@@ -0,0 +1,12 @@
+class CreateSiteSurveysAfterUser < ActiveRecord::Migration
+  def change
+    create_table :site_surveys do |t|
+      t.string :name
+      t.text :description
+      t.references :user, index: true, foreign_key: true
+      t.references :organzation_site, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
